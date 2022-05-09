843a32f252bf1b4b9303cd0f0d9e7030dae34a9a
nexxyb <nexxyb@gmail.com>
2022-05-09 12:28:43 +0100

    Create app.py
    This is a plotly dashboard app

---
+++ app.py
@@ -0,0 +1,32 @@
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.read_csv("Final_sales.csv")
+df0 = df.sort_values(by= "date")
+
+fig = px.line(df0, x="date", y="sales", color="region")
+
+app.layout = html.Div(children=[
+    html.H1(children="Soul's Food"),
+
+    html.Div(children='''
+        Pink Morsel Sales Analysis Dashboard.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
