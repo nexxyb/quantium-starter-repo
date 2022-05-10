b92a77eddf1e45bf5343868bcbc2ce147cbdb9a6
nexxyb <nexxyb@gmail.com>
2022-05-10 15:03:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,84 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Output, Input
 import plotly.express as px
 import pandas as pd
-
+#nitialize dash
 app = Dash(__name__)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
+#read data
 df = pd.read_csv("Final_sales.csv")
 df0 = df.sort_values(by= "date")
 
-fig = px.line(df0, x="date", y="sales", color="region")
+COLORS={'primary':'#FEDBFF',
+        'secondary':'#D598EB',
+        'font':'#522A61'}
 
-app.layout = html.Div(children=[
-    html.H1(children="Soul's Food", style={'textAlign':'center'}),
+#create visualization
+def generate_figure(chart_data):
+    fig = px.line(chart_data, x="date", y="sales", title="Pink Morsel Sales")
+    fig.update_layout(plot_bgcolor=COLORS['secondary'],
+        paper_bgcolor=COLORS['primary'],
+        font_color=COLORS["font"])
+    return fig
 
-    html.Div(children='''
-        Pink Morsel Sales Analysis Dashboard.
-    ''', style={'textAlign':'center'}),
+visualization= dcc.Graph(
+    id="visualization",
+    figure=generate_figure(df0)    )
 
-    html.Br(),
-    html.Label('Radio Items'),
-    dcc.RadioItems(['North', 'South', 'East', 'West','All'], style={'padding':10, 'flex':1}),
+#create the header
+header= html.H1(
+    "Pink Morsel Visualizer",
+    id="header",
+    style={
+        "background-color": COLORS["secondary"],
+        "color": COLORS["font"],
+        "border-radius": "20px"
+    }
+)
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
-])
+#region picker
+region_picker= dcc.RadioItems(
+    ["north", "east", "south", "west", "all"],
+    "north",
+    id= "region_picker",
+    inline=True
+
+)
+
+region_picker_wrapper= html.Div(
+    [region_picker],
+    style= {"font-size": "150%"}
+)
+
+#define the region picker callback
+@app.callback(
+    Output(visualization, "figure"),
+    Input(region_picker, "value")
+)
+def update_graph(region):
+    #filter the dataset
+    if region == "all":
+        trimmed_data= df0
+    else:
+        trimmed_data= df0[df0["region"] == region]
+
+    #generate a new line chart with filtered data
+    figure = generate_figure(trimmed_data)
+    return figure
+#define app layout
+app.layout= html.Div(
+    [header,
+    visualization,
+    region_picker_wrapper],
+    style={"textAlign": "center",
+        "background-color": COLORS["primary"],
+        "border-radius": "20px"}
+)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
